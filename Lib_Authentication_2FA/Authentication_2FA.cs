9be23fbe3d3f90cd12410965dcb6abc1edffdd39
Gustavo <gustavopereirafacal@gmail.com>
2024-02-03 19:38:44 -0300

    tirando algumas responsabilidades do gerarQR

--- Lib_Authentication_2FA/Authentication_2FA.cs
+++ Lib_Authentication_2FA/Authentication_2FA.cs
@@ -1,94 +1,86 @@
-﻿using Google.Authenticator;
-using Lib_Authentication_2FA.Interface;
-using System.Security.Cryptography;
+﻿    using Google.Authenticator;
+    using System.Security.Cryptography;
 
-namespace Lib_Authentication_2FA;
+    namespace Lib_Authentication_2FA;
 
-public class Authentication_2FA
-{
-    private readonly TwoFactorAuthenticator _tfa;
-    private readonly IKeyStorageService _keyStorageService;
-
-    public Authentication_2FA(IKeyStorageService keyStorageService)
+    public class Authentication_2FA
     {
-        _tfa = new TwoFactorAuthenticator();
-        _keyStorageService = keyStorageService ?? throw new ArgumentNullException(nameof(keyStorageService));
-    }
+        private readonly TwoFactorAuthenticator _tfa;
 
-    /// <summary>
-    /// Gera uma chave secreta para autenticação de dois fatores.
-    /// </summary>
-    /// <returns>A chave secreta deve ser gerada.</returns>
-    private string GenerateSecretKey(int keySize)
-    {
-        if (keySize <= 0)
+        public Authentication_2FA()
         {
-            throw new ArgumentException("O tamanho da chave deve ser maior que zero.");
+            _tfa = new TwoFactorAuthenticator();
         }
 
-        var generateNumber = RandomNumberGenerator.Create();
-
-        byte[] randomBytes = new byte[keySize];
-        generateNumber.GetBytes(randomBytes);
-
-        return BitConverter.ToString(randomBytes).Replace("-", string.Empty);
-    }
-
-    /// <summary>
-    /// Gera um código QR e salva a chave no banco de dados para um usuário.
-    /// </summary>
-    /// <param name="email">O email do usuário.</param>
-    /// <param name="keySize">Defina o tamanho da chave gerada </param>
-    /// <param name="codeDurationSeconds"></param>
-    /// <param name="issuer">O emissor da chave (padrão é "MyApp").</param>
-    /// <returns>O URL do código QR gerado.</returns>
-    public string GenerateQR(string email, int keySize = 10, string issuer = "MyApp", int codeDurationSeconds = 3)
-    {
-        try
+        /// <summary>
+        /// Gera uma chave secreta para autenticação de dois fatores.
+        /// </summary>
+        /// <returns>A chave secreta deve ser gerada.</returns>
+        public string GenerateSecretKey(int keySize = 10)
         {
-            if (string.IsNullOrEmpty(email))
+            if (keySize <= 0)
             {
-                throw new ArgumentException("O email do usuário não pode ser nulo ou vazio.");
+                throw new ArgumentException("O tamanho da chave deve ser maior que zero.");
             }
 
-            string key = GenerateSecretKey(keySize);
+            var generateNumber = RandomNumberGenerator.Create();
 
-            // Salvar a chave associada ao usuário
-            _keyStorageService.SaveKey(email, key);
+            byte[] randomBytes = new byte[keySize];
+            generateNumber.GetBytes(randomBytes);
 
-            SetupCode setupInfo = _tfa.GenerateSetupCode(issuer, email, key, false, codeDurationSeconds);
-
-            return setupInfo.QrCodeSetupImageUrl;
+            return BitConverter.ToString(randomBytes).Replace("-", string.Empty);
         }
-        catch (Exception ex)
-        {
-            // Trate a exceção ou registre-a, dependendo dos requisitos do seu aplicativo.
-            throw new ArgumentException($"Erro ao gerar código QR: {ex.Message}");
-        }
-    }
 
-    /// <summary>
-    /// Valida um código de autenticação de dois fatores.
-    /// </summary>
-    /// <param name="key">Chave atrelado ao usuario</param>
-    /// <param name="code">O código inserido pelo usuário.</param>
-    /// <returns>True se o código for válido, False caso contrário.</returns>
-    public bool ValidateCode(string code, string key)
-    {
-        try
+        /// <summary>
+        /// Gera um código QR e salva a chave no banco de dados para um usuário.
+        /// </summary>
+        /// <param name="email">O email do usuário.</param>
+        /// <param name="keySize">Defina o tamanho da chave gerada </param>
+        /// <param name="codeDurationSeconds"></param>
+        /// <param name="issuer">O emissor da chave (padrão é "MyApp").</param>
+        /// <returns>O URL do código QR gerado.</returns>
+        public string GenerateQR(string email, string keySize, string issuer = "MyApp", int codeDurationSeconds = 3)
         {
-            if (string.IsNullOrEmpty(code) || string.IsNullOrEmpty(key))
+            try
             {
-                throw new ArgumentException("O código e a chave não podem ser nulos ou vazios.");
-            }
+                if (string.IsNullOrEmpty(email))
+                {
+                    throw new ArgumentException("O email do usuário não pode ser nulo ou vazio.");
+                }
 
-            return _tfa.ValidateTwoFactorPIN(key, code);
+                SetupCode setupInfo = _tfa.GenerateSetupCode(issuer, email, keySize, false, codeDurationSeconds);
+
+                return setupInfo.QrCodeSetupImageUrl;
+            }
+            catch (Exception ex)
+            {
+                // Trate a exceção ou registre-a, dependendo dos requisitos do seu aplicativo.
+                throw new ArgumentException($"Erro ao gerar código QR: {ex.Message}");
+            }
         }
-        catch (Exception ex)
+
+        /// <summary>
+        /// Valida um código de autenticação de dois fatores.
+        /// </summary>
+        /// <param name="key">Chave atrelado ao usuario</param>
+        /// <param name="code">O código inserido pelo usuário.</param>
+        /// <returns>True se o código for válido, False caso contrário.</returns>
+        public bool  ValidateCode(string code, string key)
         {
-            // Logar a exceção ou tratar conforme necessário.
-            Console.WriteLine($"Erro ao validar código de autenticação: {ex.Message}");
-            return false;
+            try
+            {
+                if (string.IsNullOrEmpty(code) || string.IsNullOrEmpty(key))
+                {
+                    throw new ArgumentException("O código e a chave não podem ser nulos ou vazios.");
+                }
+
+                return _tfa.ValidateTwoFactorPIN(key, code);
+            }
+            catch (Exception ex)
+            {
+                // Logar a exceção ou tratar conforme necessário.
+                Console.WriteLine($"Erro ao validar código de autenticação: {ex.Message}");
+                return false;
+            }
         }
     }
-}
